b637eb8af2a20b9a288a0354f1988d521faee677
mvozian1990 <83980818+mvozian1990@users.noreply.github.com>
2021-12-06 19:46:30 -0800

    Ping and connected using ssh

--- Server.py
+++ Server.py
@@ -1,10 +1,39 @@
-class Server:
-    """ Server class for representing and manipulating servers. """
+import os
+import paramiko
 
-    def __init__(self, server_ip):
+
+class Server:
+    # """ Server class for representing and manipulating servers. """
+
+    def __init__(self, server_ip, key_file, username, upgrade_command):
         # TODO -
+        self.username = username
+        self.command = upgrade_command
+        self.key_file = key_file
         self.server_ip = server_ip
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        result = os.system("ping -c 3 %s" % self.server_ip)
+        return result
+
+    def upgrade(self):
+        # TODO - Use os module to ping the server
+
+        # Create the ssh client
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        k = paramiko.RSAKey.from_private_key_file(self.key_file)
+        # Make the connection to the server
+        ssh_client.connect(hostname=self.server_ip, username=self.username, pkey=k)
+
+        # execute the command
+        stdin, stdout, stderr = ssh_client.exec_command(self.command)
+
+        # Return the result from both stdout and error
+        result = stdout.readlines() + stderr.readlines()
+
+        # Disconnect
+        ssh_client.close()
+
+        return result
--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 # This is the template code for the CNA337 Final Project
 # Zachary Rubin, zrubin@rtc.edu
 # CNA 337 Fall 2020
+from Server import Server
+
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Mariana Vozian")
 
-# This is the entry point to our program
-if __name__ == '__main__':
-    print_program_info()
-    # TODO - Create a Server object
-    # TODO - Call Ping method and print the results
+
+class Main:
+
+    # This is the entry point to our program
+    if __name__ == '__main__':
+        print_program_info()
+        # TODO - Create a Server object
+        # TODO - Call Ping method and print the results
+
+        my_server_ip = "18.221.28.248"
+        my_rsa_key_file = "/Users/mariana/Downloads/MVCNE_key.pem"
+        username = "ec2-user"
+
+        my_upgrade_command = 'sudo yum update && sudo yum upgrade -y'
+        my_server = Server(my_server_ip, my_rsa_key_file, username, my_upgrade_command)
+
+        print('Pinging server %s...' % my_server_ip)
+        ping_result = my_server.ping()
+        print(ping_result)
+        if ping_result == 0:
+            print("Pinging IP [%s] successful." % my_server_ip)
+        else:
+            print("Pinging IP [%s] Failed." % my_server_ip)
+
+        print("\nUpdating server using ssh client from paramiko...")
+        ssh_result = my_server.upgrade()
+        print(''.join(ssh_result))
+
+        print('Done.')
